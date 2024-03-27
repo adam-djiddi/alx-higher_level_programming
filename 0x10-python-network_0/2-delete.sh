9c74ccbf142e8e015e9c1d0a886c2dda96413618
adam-djiddi <a.d.c.haggar@gmail.com>
2024-03-27 17:50:20 +0200

    net

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the response body for a given URL for 200 status code responses.
-curl -sL "$1"
+# Send a DELETE request to a given URL and display the response body.
+curl -sX DELETE "$1"
